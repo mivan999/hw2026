d75343828777623e78996cb3fcd18ceaf8828dbe
ivan <ivan@MacBook-Air-ivan.local>
2026-02-03 17:04:20 +0700

    hw2

--- src/HW/2/CurrentUser.tsx
+++ src/HW/2/CurrentUser.tsx
@@ -1,14 +1,23 @@
-import { UserType } from './HW2';
+import {UserType} from './HW2';
 
-type CurrentUserPropsType = {
-  user: any // пропиши типизацию
+export type CurrentUserPropsType = {
+    user:
+        {
+            id: number
+            name: string
+            age: number
+            address: {
+                street: string
+                city: string
+            }
+        }
 };
 
 export const CurrentUser = (props: CurrentUserPropsType) => {
-  return (
-    <li key={props.user.id} id={`hw02-user-${props.user.id}`}>
-      <strong>{props.user.name}</strong> (Age: {props.user.age})<strong> Address: </strong>
-      {props.user.address.street}, {props.user.address.city}
-    </li>
-  );
+    return (
+        <li key={props.user.id} id={`hw02-user-${props.user.id}`}>
+            <strong>{props.user.name}</strong> (Age: {props.user.age})<strong> Address: </strong>
+            {props.user.address.street}, {props.user.address.city}
+        </li>
+    );
 };
